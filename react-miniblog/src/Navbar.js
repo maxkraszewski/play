ada5b166db04714fd451d175f917bbc7ace4625b
Max Kraszewski <max.kraszewski@gmail.com>
2021-10-16 14:18:47 -0300

    Components

---
+++ react-miniblog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (  
+    <nav className="navbar">
+      <h1>The Mini Blog</h1>
+      <div className="links">
+        <a href="/">Inicio</a>
+        <a href="/creaate">Nuevo</a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
