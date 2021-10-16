34ccd7fb23ced9608f0d8e03bb82f18d8474b1ed
Max Kraszewski <max.kraszewski@gmail.com>
2021-10-16 20:54:24 -0300

    useFecth cleanup

--- react-miniblog/src/useFetch.js
+++ react-miniblog/src/useFetch.js
@@ -6,7 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
+    
+    const abortCont = new AbortController();
+    
+    fetch(url, { signal: abortCont.signal})
       .then((res) => {
         if (!res.ok) {
           // error coming back from server
@@ -20,10 +23,17 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
-        // auto catches network / connection error
-        setIsPending(false);
-        setError(err.message);
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted');
+
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
       });
+
+      return () => abortCont.abort();
+
   }, [url]);
 
   return {data, isPending, error}
