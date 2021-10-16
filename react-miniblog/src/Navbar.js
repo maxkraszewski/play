3ce1f63b0bf85ade798366ad027d3b20faff3518
Max Kraszewski <max.kraszewski@gmail.com>
2021-10-16 20:47:03 -0300

    Link

--- react-miniblog/src/Navbar.js
+++ react-miniblog/src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (  
     <nav className="navbar">
       <h1>The Mini Blog</h1>
       <div className="links">
-        <a href="/">Inicio</a>
-        <a href="/create">Nuevo</a>
+        <Link to="/">Inicio</Link>
+        <Link to="/create">Nuevo</Link>
       </div>
     </nav>
   );
