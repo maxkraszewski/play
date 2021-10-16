fc58b144a95db81204034be76e9cd533f58262ab
Max Kraszewski <max.kraszewski@gmail.com>
2021-10-16 20:25:05 -0300

    Hook Components

--- react-miniblog/src/Home.js
+++ react-miniblog/src/Home.js
@@ -1,32 +1,8 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            // error coming back from server
-            throw Error("could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setIsPending(false);
-          setBlogs(data);
-          setError(null);
-        })
-        .catch((err) => {
-          // auto catches network / connection error
-          setIsPending(false);
-          setError(err.message);
-        });
-
-  }, []);
+  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
