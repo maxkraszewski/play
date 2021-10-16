747d849d1cd7f07319fee09a3d3fffb6900c111c
Max Kraszewski <max.kraszewski@gmail.com>
2021-10-16 20:43:27 -0300

    Routes with Switch

---
+++ react-miniblog/src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return ( 
+    <div className="create">
+      <h2>Agregar Nota</h2>
+    </div>
+   );
+}
+ 
+export default Create;
--- react-miniblog/src/Navbar.js
+++ react-miniblog/src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
       <h1>The Mini Blog</h1>
       <div className="links">
         <a href="/">Inicio</a>
-        <a href="/creaate">Nuevo</a>
+        <a href="/create">Nuevo</a>
       </div>
     </nav>
   );
