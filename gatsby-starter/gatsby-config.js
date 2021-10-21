c86f2f515c47b70a1b40558ed2f60316dc9e0bdd
Max Kraszewski <max.kraszewski@gmail.com>
2021-10-20 23:11:22 -0300

    GraphQL Page Query

--- gatsby-starter/gatsby-config.js
+++ gatsby-starter/gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Starter',
+    description: 'Starter web dev portfolio',
+    copyright: 'This website is copyright 2021'
+  }, 
 }
